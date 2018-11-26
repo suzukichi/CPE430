d6a6a1ef840321b80ad2c5f25a8437b8038964cb
Suzukichi <dmchiu@calpoly.edu>
2018-11-26 15:59:38 -0800

    Add records
    They don't compile, but I'll take a look later.

--- asgn6.erl
+++ asgn6.erl
@@ -1,5 +1,20 @@
 -module(asgn6).
 -export([hello_world/0]).
 
+% ExprCs
+% TODO: ExprC (U NumC IdC StrC IfC LamC AppC))
+-record(NumC, {val}). 
+-record(IdC, {name}).
+-record(StrC, {val}).
+-record(IfC, {test, then, else}).
+-record(LamC, {args, body}).
+-record(AppC, {func, args}).
+
+% TODO: Interp
+
+
+% TODO: Parse
+
+
 hello_world() -> io:fwrite("hello, world\n").
    
