f8d4afef4910bdc82e70779bab52bc2c5406f3de
Suzukichi <dmchiu@calpoly.edu>
2018-11-26 18:37:38 -0800

    Add ExprC types and some test cases
    Sets up how to access fields of records.

--- asgn6.erl
+++ asgn6.erl
@@ -1,14 +1,15 @@
 -module(asgn6).
--export([hello_world/0]).
+-export([test/0]).
 
 % ExprCs
+% Note in Erlang, records must have a lowercase name (unknown why).
 % TODO: ExprC (U NumC IdC StrC IfC LamC AppC))
--record(NumC, {val}). 
--record(IdC, {name}).
--record(StrC, {val}).
--record(IfC, {test, then, else}).
--record(LamC, {args, body}).
--record(AppC, {func, args}).
+-record(numC, {val}). 
+-record(idC, {name}).
+-record(strC, {val}).
+-record(ifC, {test, then, else}).
+-record(lamC, {args, body}).
+-record(appC, {func, args}).
 
 % TODO: Interp
 
@@ -16,5 +17,13 @@
 % TODO: Parse
 
 
-hello_world() -> io:fwrite("hello, world\n").
-   
+% Test cases
+test() ->
+   % Basic ExprCs
+   12 = #numC{val=12}#numC.val,
+   $d = #idC{name=$d}#idC.name,
+   "meow" = #strC{val="meow"}#strC.val,
+   true = #ifC{test=true, then=true, else=false}#ifC.test,
+   {#idC{name=$a}, #idC{name=$b}} = #lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}#lamC.args,
+   {#numC{val=12}, #numC{val=3}} = #appC{func=#lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}, args={#numC{val=12}, #numC{val=3}}}#appC.args,
+   passed.
