8989e10a2130d901c0654dafa3d23481a395c23d
oscar <objones@calpoly.edu>
2018-11-27 22:26:00 -0800

    added appc functionality to interp, primopV's, closV's, and tests

--- asgn6.erl
+++ asgn6.erl
@@ -6,8 +6,6 @@
 % Note: in Erlang, records must have a lowercase name.
 % Variables have capital letter names.
 
-%TODO: values, actually getting interp working with ifc appc
-
 % ExprCs
 -record(numC, {val}). 
 -record(idC, {name}).
@@ -15,7 +13,12 @@
 -record(ifC, {test, then, else}).
 -record(lamC, {args, body}).
 -record(appC, {func, args}).
--type exprC() :: #numC{} | #idC{} | #strC{} | #ifC{} | #lamC{} | #appC{}.
+-record(closV, {args, body, env}).
+-record(primopV, {op}).
+%TODO: boolV's (needed to get interp working with appC's of ifC's)
+
+% appears to not be needed:
+%-type exprC() :: #numC{} | #idC{} | #strC{} | #ifC{} | #lamC{} | #appC{}.
 
 % Interprets an expression with a given environment
 interp(Expr, Env) ->
@@ -23,25 +26,33 @@ interp(Expr, Env) ->
       #numC{val=N} -> N;
       #strC{val=S} -> S;
       #idC{name=Id} -> lookupEnv(Id, Env);
-      % A lamC is really no different from a cloV
-      #lamC{args=Args, body=Body} -> Expr;
+      #lamC{args=Args, body=Body} -> #closV{args=Args, body=Body, env=Env};
       #ifC{test=Test, then=Then, else=Else} ->
          case interp(Test, Env) of
             true -> interp(Then, Env);
             false -> interp(Else, Env)
          end;
-      #appC{func=Func, args=Args} -> "todo"
+      #appC{func=Func, args=Args} ->
+         case interp(Func, Env) of
+            #primopV{op=Op} ->
+               Op(interp(lists:nth(1, Args), Env), interp(lists:nth(2, Args), Env));
+            #closV{args=Cargs, body=Cbody, env=Cenv} ->
+               Argvals=lists:map(fun(Arg) -> interp(Arg, Env) end, Args),
+               Env2=multiExtendEnv(Cargs, Argvals, Cenv),
+               interp(Cbody, Env2)
+            end
    end.
-
 
 % TODO: Parse
 
 % Creates a new top env
 newTopEnv()->
-   #{'+' => fun(X, Y) -> X + Y end, '-' => fun(X, Y) -> X - Y end, 
-      '*' => fun(X, Y) -> X * Y end, '/' => fun(X, Y) -> X / Y end,
-      '<=' => fun(X, Y) -> if X =< Y -> true; X > Y -> false end end,
-      'equal?' => fun(X, Y) -> 
+   #{'+' => #primopV{op=fun(X, Y) -> X + Y end},
+      '-' => #primopV{op=fun(X, Y) -> X - Y end}, 
+      '*' => #primopV{op=fun(X, Y) -> X * Y end},
+      '/' => #primopV{op=fun(X, Y) -> X / Y end},
+      '<=' => #primopV{op=fun(X, Y) -> if X =< Y -> true; X > Y -> false end end},
+      'equal?' => #primopV{op=fun(X, Y) -> 
          if 
             is_boolean(X) and is_boolean(Y) ->
                X == Y;
@@ -52,7 +63,9 @@ newTopEnv()->
                X == Y;
             true ->
                false
-         end end}.
+         end end}
+      %TODO: add 'true' and 'false' to environment
+      }.
 
 % Gets the value of a binding from the environment
 lookupEnv(Needle, Haystack) ->
@@ -100,6 +113,7 @@ test()->
 
 % Basic ExprCs
 testBasic() ->
+   io:fwrite("\trunning basic tests\n"),
    12 = #numC{val=12}#numC.val,
    $d = #idC{name=$d}#idC.name,
    "meow" = #strC{val="meow"}#strC.val,
@@ -110,31 +124,54 @@ testBasic() ->
 
 % Basic Environment Tests
 testEnv() ->
-   12 = apply(lookupEnv('+', newTopEnv()), [4, 8]),
-   0 = apply(lookupEnv('-', newTopEnv()), [4, 4]),
-   16 = apply(lookupEnv('*', newTopEnv()), [4, 4]),
-   4.0 = apply(lookupEnv('/', newTopEnv()), [16, 4]),
-   false = apply(lookupEnv('equal?', newTopEnv()), [5, true]),
-   false = apply(lookupEnv('equal?', newTopEnv()), [5, 4]),
-   false = apply(lookupEnv('equal?', newTopEnv()), [true, false]),
-   false = apply(lookupEnv('equal?', newTopEnv()), ["let's get", "this bread"]),
-   true = apply(lookupEnv('equal?', newTopEnv()), ["bread", "bread"]),
-   true = apply(lookupEnv('equal?', newTopEnv()), [false, false]),
-   true = apply(lookupEnv('equal?', newTopEnv()), [5, 5]),
+   io:fwrite("\trunning Env tests\n"),
+   Topenv=newTopEnv(),
+   Plus=lookupEnv('+', Topenv),
+   Minus=lookupEnv('-', Topenv),
+   Multiply=lookupEnv('*', Topenv),
+   Divide=lookupEnv('/', Topenv),
+   Eq=lookupEnv('equal?', Topenv),
+   12 = apply(Plus#primopV.op, [4, 8]),
+   0 = apply(Minus#primopV.op, [4, 4]),
+   16 = apply(Multiply#primopV.op, [4, 4]),
+   4.0 = apply(Divide#primopV.op, [16, 4]),
+   false = apply(Eq#primopV.op, [5, true]),
+   false = apply(Eq#primopV.op, [5, 4]),
+   false = apply(Eq#primopV.op, [true, false]),
+   false = apply(Eq#primopV.op, ["let's get", "this bread"]),
+   true = apply(Eq#primopV.op, ["bread", "bread"]),
+   true = apply(Eq#primopV.op, [false, false]),
+   true = apply(Eq#primopV.op, [5, 5]),
    8 = apply(lookupEnv('double', 
-      extendEnv('double', fun(X) -> X * 2 end, newTopEnv())), [4]),
+      extendEnv('double', fun(X) -> X * 2 end, Topenv)), [4]),
    12 = apply(lookupEnv('triple', 
-      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end, fun(X) -> X * 3 end], newTopEnv())), [4]),
+      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end, fun(X) -> X * 3 end], Topenv)), [4]),
    ?assertException(throw, 'ZIBR: Id not in environment', 
-      lookupEnv('\\', newTopEnv())),
+      lookupEnv('\\', Topenv)),
    ?assertException(throw, 'ZIBR: arg length mismatch', 
-      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end], newTopEnv())),
+      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end], Topenv)),
    io:fwrite("\tSuccess Env Test\n").
 
 % Interp
 testInterp() ->
-   12 = interp(#numC{val=12}, newTopEnv()),
-   "meow" = interp(#strC{val="meow"}, newTopEnv()),
-   #lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}} = interp(#lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}, newTopEnv()),
-   % TODO: Tests for appc, ifc
+   io:fwrite("\trunning Interp tests\n"),
+   Topenv=newTopEnv(),
+   12 = interp(#numC{val=12}, Topenv),
+   "meow" = interp(#strC{val="meow"}, Topenv),
+   #closV{args=['Symbol1', 'Symbol2'], body=#numC{val=8}, env=Topenv} = interp(#lamC{args=['Symbol1', 'Symbol2'], body=#numC{val=8}}, Topenv),
+   3 = interp(#appC{func=#idC{name='+'}, args=[#numC{val=1}, #numC{val=2}]}, Topenv),
+   8 = interp(#appC{func=#lamC{args=[], body=#numC{val=8}}, args=[]}, Topenv),
+   7 = interp(#appC{func=#lamC{args=['Z'], body=#idC{name='Z'}}, args=[#numC{val=7}]}, Topenv),
+   12 = interp(#appC{
+                  func=#lamC{
+                           args=['ValToBeTripled'],
+                           body=#appC{
+                              func=#idC{name='*'},
+                              args=[#idC{name='ValToBeTripled'}, #numC{val=3}]
+                           }
+                        },
+                  args=[#numC{val=4}]
+               }, 
+               Topenv),
+   % TODO: Tests for ifc
    io:fwrite("\tSuccess Interp Test\n").
