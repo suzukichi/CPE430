fde97dc1dd1a481d3ad13508492d71c7a9be34e9
Nick Papadakis <nicholas.t.papadakis@gmail.com>
2018-11-27 18:55:08 -0800

    Added an environment with primops and helper functions for manipulating it with full test coverage

--- asgn6.erl
+++ asgn6.erl
@@ -1,10 +1,12 @@
 -module(asgn6).
 -export([test/0]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 % Note: in Erlang, records must have a lowercase name.
 % Variables have capital letter names.
 
-%TODO: environment, values, primops.
+%TODO: values, actually getting interp working with ifc appc
 
 % ExprCs
 -record(numC, {val}). 
@@ -36,18 +38,63 @@ interp(Expr, Env) ->
 
 % Creates a new top env
 newTopEnv()->
-%s TODO: fill out
-12.
+   #{'+' => fun(X, Y) -> X + Y end, '-' => fun(X, Y) -> X - Y end, 
+      '*' => fun(X, Y) -> X * Y end, '/' => fun(X, Y) -> X / Y end,
+      '<=' => fun(X, Y) -> if X =< Y -> true; X > Y -> false end end,
+      'equal?' => fun(X, Y) -> 
+         if 
+            is_boolean(X) and is_boolean(Y) ->
+               X == Y;
+            is_number(X) and is_number(Y) ->
+               X == Y;
+            %generalizing strings to lists, which should be ok?
+            is_list(X) and is_list(Y) ->
+               X == Y;
+            true ->
+               false
+         end end}.
 
-% Gets the value of a bar from the environment
+% Gets the value of a binding from the environment
 lookupEnv(Needle, Haystack) ->
-   % TODO: actually look up.
-   Needle.
+   IsInEnv = maps:is_key(Needle, Haystack),
+
+   if
+      IsInEnv ->
+         maps:get(Needle, Haystack);
+
+      true -> %works just like an else branch
+         throw('ZIBR: Id not in environment')
+   end.
+
+% Helper function for extending the environment with a list of bindings and a list
+% of values
+multiExtendEnv(IdList, ValueList, Env) ->
+   IdListLen = length(IdList),
+   ValueListLen = length(ValueList),
+
+   if
+      IdListLen /= ValueListLen ->
+         throw('ZIBR: arg length mismatch');
+
+      IdListLen == 0 ->
+         Env;
+
+      true ->
+         [IdFirst | IdRest] = IdList,
+         [ValueFirst | ValueRest] = ValueList,
+         multiExtendEnv(IdRest, ValueRest, 
+            extendEnv(IdFirst, ValueFirst, Env))
+   end.
+
+% Returns a new environment with a new binding added to it
+extendEnv(Id, Value, Env) ->
+   maps:put(Id, Value, Env).
 
 % Test cases
 test()->
    io:fwrite("Test suite\n"),
    testBasic(),
+   testEnv(),
    testInterp(),
    io:fwrite("Test suite complete\n").
 
@@ -59,7 +106,30 @@ testBasic() ->
    true = #ifC{test=true, then=true, else=false}#ifC.test,
    {#idC{name=$a}, #idC{name=$b}} = #lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}#lamC.args,
    {#numC{val=12}, #numC{val=3}} = #appC{func=#lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}, args={#numC{val=12}, #numC{val=3}}}#appC.args,
-   io:fwrite("\tSuccess basic exprc\n").
+   io:fwrite("\tSuccess Basic ExprC Test\n").
+
+% Basic Environment Tests
+testEnv() ->
+   12 = apply(lookupEnv('+', newTopEnv()), [4, 8]),
+   0 = apply(lookupEnv('-', newTopEnv()), [4, 4]),
+   16 = apply(lookupEnv('*', newTopEnv()), [4, 4]),
+   4.0 = apply(lookupEnv('/', newTopEnv()), [16, 4]),
+   false = apply(lookupEnv('equal?', newTopEnv()), [5, true]),
+   false = apply(lookupEnv('equal?', newTopEnv()), [5, 4]),
+   false = apply(lookupEnv('equal?', newTopEnv()), [true, false]),
+   false = apply(lookupEnv('equal?', newTopEnv()), ["let's get", "this bread"]),
+   true = apply(lookupEnv('equal?', newTopEnv()), ["bread", "bread"]),
+   true = apply(lookupEnv('equal?', newTopEnv()), [false, false]),
+   true = apply(lookupEnv('equal?', newTopEnv()), [5, 5]),
+   8 = apply(lookupEnv('double', 
+      extendEnv('double', fun(X) -> X * 2 end, newTopEnv())), [4]),
+   12 = apply(lookupEnv('triple', 
+      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end, fun(X) -> X * 3 end], newTopEnv())), [4]),
+   ?assertException(throw, 'ZIBR: Id not in environment', 
+      lookupEnv('\\', newTopEnv())),
+   ?assertException(throw, 'ZIBR: arg length mismatch', 
+      multiExtendEnv(['double', 'triple'], [fun(X) -> X * 2 end], newTopEnv())),
+   io:fwrite("\tSuccess Env Test\n").
 
 % Interp
 testInterp() ->
@@ -67,4 +137,4 @@ testInterp() ->
    "meow" = interp(#strC{val="meow"}, newTopEnv()),
    #lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}} = interp(#lamC{args={#idC{name=$a}, #idC{name=$b}}, body=#numC{val=5}}, newTopEnv()),
    % TODO: Tests for appc, ifc
-   io:fwrite("\tSuccess Interp\n").
+   io:fwrite("\tSuccess Interp Test\n").
